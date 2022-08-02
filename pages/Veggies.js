b9e8eee7048ddcde9b6c4b0d296882317b9ec8dc
Vladyslav Nyzhashchyy <contact.johngoldco@gmail.com>
2022-08-02 14:38:14 +0200

    Add image to veggies page

--- pages/Veggies.js
+++
@@ -1,6 +0,0 @@
-export default function Veggies() {
-    return (<>
-        <h1>Veggies</h1>
-        <p>Oh nice! Some veggie recipes for my belly!</p>
-        </>)
-}
---
+++ pages/Veggies.jsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+
+export default function Veggies() {
+  return (
+    <>
+      <h1>Veggies</h1>
+      <p>Oh nice! Some veggie recipes for my belly!</p>
+      <Image
+        src="/../public/pizza.jpg"
+        alt="veggie pizza"
+        layout="responsive"
+        width={2400}
+        height={1596}
+      />
+    </>
+  );
+}

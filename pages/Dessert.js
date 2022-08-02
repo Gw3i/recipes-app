ded6b671333e788985629f4ab878498fddbe73fc
Vladyslav Nyzhashchyy <contact.johngoldco@gmail.com>
2022-08-02 14:16:06 +0200

    Create next.js app and add pages

---
+++ pages/Dessert.js
@@ -0,0 +1,7 @@
+export default function Dessert() {
+    return (<>
+    <h1>Dessert</h1>
+    <p>Mmmhhhh... sweet, sweet desserts</p>
+    </>
+    )
+}
---
+++ pages/Veggies.js
@@ -0,0 +1,6 @@
+export default function Veggies() {
+    return (<>
+        <h1>Veggies</h1>
+        <p>Oh nice! Some veggie recipes for my belly!</p>
+        </>)
+}
